a321c1ffebe05c309bc037717593f25fee90d9b5
Jason Melo <jason.melo@gmail.com>
2012-04-29 15:03:52 -0400

    project creation, here we go!

--- app.js
+++ app.js
@@ -1,44 +1,25 @@
 require('coffee-script');
 
-var config = require('./../../Citiology/Citiology.Node.Web/config')
+var config = require('./config')
     , express = require('express')
     , app = module.exports = express.createServer();
 
 //
 // Express Setup
-require("./../../Citiology/Citiology.Node.Web/lib/express-config.coffee")(app, __dirname);
+require("./lib/express-config.coffee")(app, __dirname);
 
 //
 // Routes
-var root = require("./../../Citiology/Citiology.Node.Web/routes/root")
-    , permissionFilter = require("./../../Citiology/Citiology.Node.Web/services/jm.permissions.coffee")
-    , interview = require("./../../Citiology/Citiology.Node.Web/routes/interview")
-    , playlist = require("./../../Citiology/Citiology.Node.Web/routes/playlist")
-    , venue = require("./../../Citiology/Citiology.Node.Web/routes/venue")
-    , user = require("./../../Citiology/Citiology.Node.Web/routes/user")
-    , intheloop = require("./../../Citiology/Citiology.Node.Web/routes/intheloop");
+var root = require("./routes/root")
+    , modeler = require("./routes/modeler");
 
-app.get('/interview',               permissionFilter.restrictToPermission('INTERVIEW_VIEW'),    interview.featured);
-app.get('/interview/:id',           permissionFilter.restrictToPermission('INTERVIEW_VIEW'),    interview.index);
+app.get('/modeler',     modeler.featured);
+app.get('/modeler/:id', modeler.index);
 
-app.get('/api/interview/type/:type',permissionFilter.restrictToPermission('INTERVIEW_VIEW'),    interview.interviewsByType);
-app.get('/api/interview/:id',       permissionFilter.restrictToPermission('INTERVIEW_VIEW'),    interview.getInterview);
+app.get('/api/modeler/type/:type',  modeler.interviewsByType);
+app.get('/api/modeler/:id',  modeler.getInterview);
 
-app.get('/playlist',                permissionFilter.restrictToPermission('PLAYLIST_VIEW'),     playlist.recent);
-app.get('/playlist/:id',            permissionFilter.restrictToPermission('PLAYLIST_VIEW'),     playlist.index);
-
-app.get('/api/playlist/type/:type', permissionFilter.restrictToPermission('PLAYLIST_VIEW'),     playlist.playlistsByType);
-app.get('/api/playlist/venue/:id',  permissionFilter.restrictToPermission('PLAYLIST_VIEW'),     playlist.playlistsByVenue);
-app.get('/api/playlist/:id',        permissionFilter.restrictToPermission('PLAYLIST_VIEW'),     playlist.getPlaylist);
-
-app.get('/venue/:id',               permissionFilter.restrictToPermission('VENUE_VIEW'),        venue.index);
-app.get('/api/venue/:id',           permissionFilter.restrictToPermission('VENUE_VIEW'),        venue.getVenue);
-
-app.get('/api/user/:id',            permissionFilter.restrictToPermission('USER_VIEW'),         user.getUser);
-
-app.get('/intheloop',               permissionFilter.restrictToPermission('VENUE_VIEW'),        intheloop.index);
-
-app.all('/',                        permissionFilter.restrictToPermission('ALL_VIEW'),          interview.featured);
+app.all('/',  modeler.featured);
 
 //
 // Server Listener
